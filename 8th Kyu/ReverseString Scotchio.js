4eea9529ff9461bcf1d21cf33c20d346fbebf0f5
Virgil_HD <120189340+virgilhg@users.noreply.github.com>
2025-10-28 12:22:51 -0500

    ReverseString Scotchio.js

---
+++ 8th Kyu/ReverseString Scotchio.js
@@ -0,0 +1,9 @@
+/*
+Given a string of text, write an algorithm that returns the text received in a reversed format. E.g
+
+reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+function reverseString(text) {
+    return [...text].reduce((acc, char) => char + acc, '')
+}
